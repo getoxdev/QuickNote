aaea80ce41f36a2829c33e27e2c861823b839f41
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2020-08-06 15:12:40 +0530

    Change Log: 1. Added searchview custom from github repo. 2. MainActivity needs changed to enable search view in collapse and expanded mode

--- app/src/main/java/com/quicknote/MainActivity.java
+++ app/src/main/java/com/quicknote/MainActivity.java
@@ -1,14 +1,19 @@
 package com.quicknote;
 
+import android.animation.LayoutTransition;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ferfalk.simplesearchview.SimpleSearchView;
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.quicknote.Database.NoteEntity;
@@ -17,6 +22,7 @@ import com.quicknote.ViewModels.ListViewModel;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.lifecycle.Observer;
@@ -33,6 +39,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.collapsed_toolbar)
     CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @BindView(R.id.appbarlayout)
+    AppBarLayout appBarLayout;
 
     private List<NoteEntity> mNotesList = new ArrayList<>();
     private ListViewModel listViewModel;
@@ -144,6 +154,72 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.menu_main,menu);
+        MenuItem item = menu.findItem(R.id.search_field);
+        SimpleSearchView searchViewCollapsing = (SimpleSearchView) findViewById(R.id.searchView_collapsing);
+        searchViewCollapsing.setMenuItem(item);
+        searchViewCollapsing.setOnQueryTextListener(new SimpleSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextCleared() {
+                return false;
+            }
+        });
+
+
+        SimpleSearchView searchViewNormal = (SimpleSearchView) findViewById(R.id.searchView_normal);
+        searchViewNormal.setOnQueryTextListener(new SimpleSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextCleared() {
+                return false;
+            }
+        });
+
+
+        //TODO: enable the different search for different toolbar layouts collapsed and expanded.
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+                    // Collapsed
+                    searchViewCollapsing.setVisibility(View.GONE);
+                    searchViewNormal.setVisibility(View.VISIBLE);
+
+                } else if (verticalOffset == 0) {
+                    // Expanded
+                    searchViewNormal.setVisibility(View.GONE);
+                    searchViewCollapsing.setVisibility(View.VISIBLE);
+
+                } else {
+                    // Somewhere in between
+
+
+                }
+            }
+        });
+
+
+
+
         return true;
     }
 
